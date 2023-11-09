6ab6f59111624f58a2d550d8870defad567edd06
dilu <dilu@dtstack.com>
2023-11-09 16:51:44 +0800

    build: deploy component reviewer for code review

---
+++ docs/markdownRender__demos.230872ec.async.js
@@ -0,0 +1,35 @@
+"use strict";(self.webpackChunkdt_react_component=self.webpackChunkdt_react_component||[]).push([[6426,3958],{40820:function(E,_,e){e.r(_);var a=e(54306),u=e.n(a),t=e(50959),l=e(89647),n=e(11527);_.default=function(){var r=(0,t.useState)(""),o=u()(r,2),s=o[0],d=o[1];return(0,t.useEffect)(function(){fetch("https://cdn.jsdelivr.net/npm/dt-react-component@3.0.8/CHANGELOG.md",{method:"get"}).then(function(m){return m.text()}).then(d)},[]),(0,n.jsx)("div",{style:{maxHeight:200,overflow:"auto",marginBottom:16},children:(0,n.jsx)(l.MarkdownRender,{value:s})})}},4310:function(E,_,e){e.r(_);var a=e(54306),u=e.n(a),t=e(50959),l=e(89647),n=e(11527),r=`
+\u4EE5\u4E0B\u662F\u4E00\u6BB5 sql \u8BED\u6CD5
+
+\`\`\`sql
+ select count(*) from a;
+-- name sqltest 
+-- type sql 
+-- create time 2022-11-09 16:13:45 
+-- desc
+
+
+-- create table employees(name string);
+insert into employees values('1111');
+
+
+select * from employees
+\`\`\`
+`;_.default=function(){var o=(0,t.useState)(""),s=u()(o,2),d=s[0],m=s[1];return(0,t.useEffect)(function(){m(r)},[]),(0,n.jsx)("div",{style:{maxHeight:400,overflow:"auto",marginBottom:16},children:(0,n.jsx)(l.MarkdownRender,{dark:!0,value:d})})}},2070:function(E,_,e){e.r(_);var a=e(54306),u=e.n(a),t=e(50959),l=e(89647),n=e(11527),r=`
+\u4EE5\u4E0B\u662F\u4E00\u6BB5 sql \u8BED\u6CD5
+
+\`\`\`sql
+ select count(*) from a;
+-- name sqltest 
+-- type sql 
+-- create time 2022-11-09 16:13:45 
+-- desc
+
+
+-- create table employees(name string);
+insert into employees values('1111');
+
+
+select * from employees
+\`\`\`
+`;_.default=function(){var o=(0,t.useState)(""),s=u()(o,2),d=s[0],m=s[1];return(0,t.useEffect)(function(){m(r)},[]),(0,n.jsx)("div",{style:{maxHeight:400,overflow:"auto",marginBottom:16},children:(0,n.jsx)(l.MarkdownRender,{value:d})})}}}]);
